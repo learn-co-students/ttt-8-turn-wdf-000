56e7d492b1c0d109b82096b009b23d91f10018ac
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-07-06 02:48:15 +0000

    Added method bodies for TTT

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  index = input.to_i    #converts input string to index integer
+  index = index - 1
+end
+
 def valid_move?(array, index)
   valid = index.between?(0,8)
   if(position_taken?(array, index) == true || valid == false)
@@ -21,4 +26,21 @@ def position_taken?(array, index)
   else
     true
   end
+end
+
+def move(array, index, char = "X")
+  array[index] = char
+end
+
+def turn(array)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input = input_to_index(input)
+  if(valid_move?(array, input) == true)
+    move(array, input, "X")
+  else
+    turn(array)
+  end
+  display_board(array)
+
 end
